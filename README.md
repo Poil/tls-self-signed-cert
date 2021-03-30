e16162f3076e93930e6e9dd640ce8b83604ef413
Benjamin DUPUIS <benjamin.dupuis@fr.clara.net>
2021-03-30 15:56:24 +0200

    0.14 compat and description

--- README.md
+++ README.md
@@ -13,48 +13,52 @@ Checkout [examples](./examples) for fully functioning examples.
 
 This module doesn't require any environment variables to be set.
 
-## Input Variables
+## Inputs
 
-- `create`: [Optional] Create Module, defaults to true.
-- `name`: [Optional] Filename to write the private key data to, default to "tls-private-key".
-- `algorithm`: [Optional] The name of the algorithm to use for the key. Currently-supported values are "RSA" and "ECDSA". Defaults to "RSA".
-- `rsa_bits`: [Optional] When algorithm is "RSA", the size of the generated RSA key in bits. Defaults to "2048".
-- `ecdsa_curve`: [Optional] When algorithm is "ECDSA", the name of the elliptic curve to use. May be any one of "P224", "P256", "P384" or "P521". Defaults to "P224".
-- `permissions`: [Optional] The Unix file permission to assign to the cert files (e.g. 0600), defaults to "0600".
-- `validity_period_hours`: [Required] The number of hours after initial issuing that the certificate will become invalid.
-- `ca_allowed_uses`: [Optional] List of keywords from RFC5280 describing a use that is permitted for the CA certificate. For more info and the list of keywords, see https://www.terraform.io/docs/providers/tls/r/self_signed_cert.html#allowed_uses.
-- `ca_common_name`: [Optional] The common name to use in the subject of the CA certificate (e.g. hashicorp.com).
-- `organization_name`: [Required] The name of the organization to associate with the certificates (e.g. HashiCorp Inc.).
-- `allowed_uses`: [Required] List of keywords from RFC5280 describing a use that is permitted for the issued certificate. For more info and the list of keywords, see https://www.terraform.io/docs/providers/tls/r/self_signed_cert.html#allowed_uses.
-- `common_name`: [Required] The common name to use in the subject of the certificate (e.g. hashicorp.com).
-- `dns_names`: [Required] List of DNS names for which the certificate will be valid (e.g. foo.hashicorp.com), defaults to empty list.
-- `ip_addresses`: [Required] List of IP addresses for which the certificate will be valid (e.g. 127.0.0.1), defaults to empty list.
-- `ca_override`: [Optional] Don't create a CA cert, override with the provided CA to sign certs withr
-- `ca_key_override`: [Optional] CA private key pem override.
-- `ca_cert_override`: [Optional] CA cert pem override.
-- `download_certs`: [Optional] Download certs locally, defaults to false.
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| algorithm | The name of the algorithm to use for the key. Currently-supported values are "RSA" and "ECDSA". Defaults to "RSA". | `string` | `"RSA"` | no |
+| allowed\_uses | List of keywords from RFC5280 describing a use that is permitted for the issued certificate. For more info and the list of keywords, see https://www.terraform.io/docs/providers/tls/r/self_signed_cert.html#allowed_uses. | `list` | <pre>[<br>  "key_encipherment",<br>  "digital_signature"<br>]</pre> | no |
+| ca\_allowed\_uses | List of keywords from RFC5280 describing a use that is permitted for the CA certificate. For more info and the list of keywords, see https://www.terraform.io/docs/providers/tls/r/self_signed_cert.html#allowed_uses. | `list` | <pre>[<br>  "cert_signing",<br>  "key_encipherment",<br>  "digital_signature"<br>]</pre> | no |
+| ca\_cert\_override | CA cert pem override. | `string` | `""` | no |
+| ca\_common\_name | The common name to use in the subject of the CA certificate (e.g. hashicorp.com). | `string` | `""` | no |
+| ca\_key\_override | CA private key pem override. | `string` | `""` | no |
+| ca\_override | Don't create a CA cert, override with the provided CA to sign certs with. | `bool` | `false` | no |
+| common\_name | The common name to use in the subject of the certificate (e.g. hashicorp.com). | `any` | n/a | yes |
+| create | Create Module, defaults to true. | `bool` | `true` | no |
+| dns\_names | List of DNS names for which the certificate will be valid (e.g. foo.hashicorp.com), defaults to empty list. | `list` | `[]` | no |
+| download\_certs | Download certs locally, defaults to false. | `bool` | `false` | no |
+| ecdsa\_curve | When algorithm is "ECDSA", the name of the elliptic curve to use. May be any one of "P224", "P256", "P384" or "P521". Defaults to "P224" | `string` | `"P256"` | no |
+| ip\_addresses | List of IP addresses for which the certificate will be valid (e.g. 127.0.0.1), defaults to empty list. | `list` | `[]` | no |
+| name | Filename to write the certificate data to, default to "tls-self-signed-cert". | `string` | `"tls-self-signed-cert"` | no |
+| organization\_name | The name of the organization to associate with the certificates (e.g. HashiCorp Inc). | `any` | n/a | yes |
+| permissions | The Unix file permission to assign to the cert files (e.g. 0600). Defaults to "0600". | `string` | `"0600"` | no |
+| rsa\_bits | When algorithm is "RSA", the size of the generated RSA key in bits. Defaults to "2048". | `string` | `"2048"` | no |
+| validity\_period\_hours | The number of hours after initial issuing that the certificate will become invalid. | `any` | n/a | yes |
 
 ## Outputs
 
-- `algorithm`: The algorithm that was selected for the key.
-- `ca_cert_pem`: The CA cert data in PEM format.
-- `ca_cert_validity_start_time`: The time after which the CA certificate is valid, as an RFC3339 timestamp.
-- `ca_cert_validity_end_time`: The time until which the CA certificate is invalid, as an RFC3339 timestamp.
-- `ca_private_key_pem`: The CA cert private key data in PEM format.
-- `ca_private_key_name`: The CA cert private key filename.
-- `ca_private_key_filename`: The CA cert private key filename with file extension.
-- `ca_public_key_pem`: The CA cert public key data in PEM format.
-- `ca_public_key_openssh`: The CA cert public key data in OpenSSH authorized_keys format, if the selected private key format is compatible. All RSA keys are supported, and ECDSA keys with curves "P256", "P384" and "P251" are supported. This attribute is empty if an incompatible ECDSA curve is selected.
-
-- `leaf_private_key_pem`: The Leaf cert private key data in PEM format.
-- `leaf_private_key_name`: The Leaf cert private key filename.
-- `leaf_private_key_filename`: The Leaf cert private key filename with file extension.
-- `leaf_public_key_pem`: The Leaf cert public key data in PEM format.
-- `leaf_public_key_openssh`: The Leaf cert public key data in OpenSSH authorized_keys format, if the selected private key format is compatible. All RSA keys are supported, and ECDSA keys with curves "P256", "P384" and "P251" are supported. This attribute is empty if an incompatible ECDSA curve is selected.
-- `leaf_cert_request_pem`: The Leaf cert request data in PEM format.
-- `leaf_cert_pem`: The Leaf cert data in PEM format.
-- `leaf_cert_validity_start_time`: The time after which the leaf certificate is valid, as an RFC3339 timestamp.
-- `leaf_cert_validity_end_time`: The time until which the leaf certificate is invalid, as an RFC3339 timestamp.
+| Name | Description |
+|------|-------------|
+| algorithm | The algorithm that was selected for the key. |
+| ca\_cert\_filename | The CA cert filename. |
+| ca\_cert\_name | The CA cert name. |
+| ca\_cert\_pem | The CA cert data in PEM format. |
+| ca\_cert\_validity\_end\_time | The time until which the CA certificate is invalid, as an RFC3339 timestamp. |
+| ca\_cert\_validity\_start\_time | The time after which the CA certificate is valid, as an RFC3339 timestamp. |
+| ca\_private\_key\_pem | The CA cert private key data in PEM format. |
+| ca\_public\_key\_openssh | The CA cert public key data in OpenSSH authorized\_keys format, if the selected private key format is compatible. All RSA keys are supported, and ECDSA keys with curves "P256", "P384" and "P251" are supported. This attribute is empty if an incompatible ECDSA curve is selected. |
+| ca\_public\_key\_pem | The CA cert public key data in PEM format. |
+| leaf\_cert\_filename | The Leaf cert filename with file extension. |
+| leaf\_cert\_name | The Leaf cert name. |
+| leaf\_cert\_pem | The Leaf cert data in PEM format. |
+| leaf\_cert\_request\_pem | The Leaf cert request data in PEM format. |
+| leaf\_cert\_validity\_end\_time | The time until which the leaf certificate is invalid, as an RFC3339 timestamp. |
+| leaf\_cert\_validity\_start\_time | The time after which the leaf certificate is valid, as an RFC3339 timestamp. |
+| leaf\_private\_key\_filename | The Leaf cert private key filename with file extension. |
+| leaf\_private\_key\_pem | The Leaf cert private key data in PEM format. |
+| leaf\_public\_key\_openssh | The Leaf cert public key data in OpenSSH authorized\_keys format, if the selected private key format is compatible. All RSA keys are supported, and ECDSA keys with curves "P256", "P384" and "P251" are supported. This attribute is empty if an incompatible ECDSA curve is selected. |
+| leaf\_public\_key\_pem | The Leaf cert public key data in PEM format. |
 
 ## Submodules
 
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "zREADME" {
-  value = <<README
+  value = <<-README
 # ------------------------------------------------------------------------------
 # ${var.name} TLS Self Signed Certs
 # ------------------------------------------------------------------------------
@@ -9,9 +9,8 @@ The below private keys and self signed TLS certificates have been generated.
 - CA certificate: ${element(concat(formatlist("%s-ca", random_id.name.*.hex), list("")), 0)}
 - Leaf certificate: ${element(concat(formatlist("%s-leaf", random_id.name.*.hex), list("")), 0)}
 
-${var.download_certs ?
-"The below certificates and private key have been downloaded locally with the
-file permissions updated appropriately.
+${var.download_certs ? <<-INNERA
+The below certificates and private key have been downloaded locally with the file permissions updated appropriately.
 
 - ${element(concat(formatlist("%s-ca.crt.pem", random_id.name.*.hex), list("")), 0)}
 - ${element(concat(formatlist("%s-leaf.crt.pem", random_id.name.*.hex), list("")), 0)}
@@ -22,90 +21,112 @@ file permissions updated appropriately.
   $ openssl x509 -text -in ${element(concat(formatlist("%s-leaf.crt.pem", random_id.name.*.hex), list("")), 0)}
 
   # Verify root CA
-  $ openssl verify -CAfile ${element(concat(formatlist("%s-ca.crt.pem", random_id.name.*.hex), list("")), 0)} \\
-    ${element(concat(formatlist("%s-leaf.crt.pem", random_id.name.*.hex), list("")), 0)}"
-:
-"Certs were not downloaded locally. set \"download_certs\" to true to download."}
+  $ openssl verify -CAfile ${element(concat(formatlist("%s-ca.crt.pem", random_id.name.*.hex), list("")), 0)} ${element(concat(formatlist("%s-leaf.crt.pem", random_id.name.*.hex), list("")), 0)}
+INNERA
+  :
+  <<-INNERB
+Certs were not downloaded locally. set 'download_certs' to true to download.
+INNERB
+}
 README
 }
 
 output "algorithm" {
-  value = "${var.algorithm}"
+  description = "The algorithm that was selected for the key."
+  value       = var.algorithm
 }
 
 # CA - TLS private key
 output "ca_private_key_pem" {
-  value = "${chomp(element(concat(tls_private_key.ca.*.private_key_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The CA cert private key data in PEM format."
+  value       = chomp(element(concat(tls_private_key.ca.*.private_key_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 output "ca_public_key_pem" {
-  value = "${chomp(element(concat(tls_private_key.ca.*.public_key_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The CA cert public key data in PEM format."
+  value       = chomp(element(concat(tls_private_key.ca.*.public_key_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 output "ca_public_key_openssh" {
-  value = "${chomp(element(concat(tls_private_key.ca.*.public_key_openssh, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The CA cert public key data in OpenSSH authorized_keys format, if the selected private key format is compatible. All RSA keys are supported, and ECDSA keys with curves \"P256\", \"P384\" and \"P251\" are supported. This attribute is empty if an incompatible ECDSA curve is selected."
+  value       = chomp(element(concat(tls_private_key.ca.*.public_key_openssh, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 # CA - TLS self signed cert
 output "ca_cert_name" {
-  value = "${element(concat(formatlist("%s-ca", random_id.name.*.hex), list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The CA cert name."
+  value       = element(concat(formatlist("%s-ca", random_id.name.*.hex), list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "ca_cert_filename" {
-  value = "${element(concat(formatlist("%s-ca.crt.pem", random_id.name.*.hex), list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The CA cert filename."
+  value       = element(concat(formatlist("%s-ca.crt.pem", random_id.name.*.hex), list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "ca_cert_pem" {
-  value = "${chomp(element(concat(tls_self_signed_cert.ca.*.cert_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The CA cert data in PEM format."
+  value       = chomp(element(concat(tls_self_signed_cert.ca.*.cert_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 output "ca_cert_validity_start_time" {
-  value = "${element(concat(tls_self_signed_cert.ca.*.validity_start_time, list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The time after which the CA certificate is valid, as an RFC3339 timestamp."
+  value       = element(concat(tls_self_signed_cert.ca.*.validity_start_time, list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "ca_cert_validity_end_time" {
-  value = "${element(concat(tls_self_signed_cert.ca.*.validity_end_time, list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The time until which the CA certificate is invalid, as an RFC3339 timestamp."
+  value       = element(concat(tls_self_signed_cert.ca.*.validity_end_time, list("")), 0) # TODO: Workaround for issue #11210
 }
 
 # Leaf - TLS private key
 output "leaf_private_key_pem" {
-  value = "${chomp(element(concat(tls_private_key.leaf.*.private_key_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert private key data in PEM format."
+  value       = chomp(element(concat(tls_private_key.leaf.*.private_key_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 output "leaf_private_key_filename" {
-  value = "${element(concat(formatlist("%s-leaf.key.pem", random_id.name.*.hex), list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert private key filename with file extension."
+  value       = element(concat(formatlist("%s-leaf.key.pem", random_id.name.*.hex), list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "leaf_public_key_pem" {
-  value = "${chomp(element(concat(tls_private_key.leaf.*.public_key_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert public key data in PEM format."
+  value       = chomp(element(concat(tls_private_key.leaf.*.public_key_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 output "leaf_public_key_openssh" {
-  value = "${chomp(element(concat(tls_private_key.leaf.*.public_key_openssh, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert public key data in OpenSSH authorized_keys format, if the selected private key format is compatible. All RSA keys are supported, and ECDSA keys with curves \"P256\", \"P384\" and \"P251\" are supported. This attribute is empty if an incompatible ECDSA curve is selected."
+  value       = chomp(element(concat(tls_private_key.leaf.*.public_key_openssh, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 # Leaf - TLS cert request
 output "leaf_cert_request_pem" {
-  value = "${chomp(element(concat(tls_cert_request.leaf.*.cert_request_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert request data in PEM format."
+  value       = chomp(element(concat(tls_cert_request.leaf.*.cert_request_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 # Leaf - TLS locally signed cert
 output "leaf_cert_name" {
-  value = "${element(concat(formatlist("%s-leaf", random_id.name.*.hex), list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert name."
+  value       = element(concat(formatlist("%s-leaf", random_id.name.*.hex), list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "leaf_cert_filename" {
-  value = "${element(concat(formatlist("%s-leaf.crt.pem", random_id.name.*.hex), list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert filename with file extension."
+  value       = element(concat(formatlist("%s-leaf.crt.pem", random_id.name.*.hex), list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "leaf_cert_pem" {
-  value = "${chomp(element(concat(tls_locally_signed_cert.leaf.*.cert_pem, list("")), 0))}" # TODO: Workaround for issue #11210
+  description = "The Leaf cert data in PEM format."
+  value       = chomp(element(concat(tls_locally_signed_cert.leaf.*.cert_pem, list("")), 0)) # TODO: Workaround for issue #11210
 }
 
 output "leaf_cert_validity_start_time" {
-  value = "${element(concat(tls_locally_signed_cert.leaf.*.validity_start_time, list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The time after which the leaf certificate is valid, as an RFC3339 timestamp."
+  value       = element(concat(tls_locally_signed_cert.leaf.*.validity_start_time, list("")), 0) # TODO: Workaround for issue #11210
 }
 
 output "leaf_cert_validity_end_time" {
-  value = "${element(concat(tls_locally_signed_cert.leaf.*.validity_end_time, list("")), 0)}" # TODO: Workaround for issue #11210
+  description = "The time until which the leaf certificate is invalid, as an RFC3339 timestamp."
+  value       = element(concat(tls_locally_signed_cert.leaf.*.validity_end_time, list("")), 0) # TODO: Workaround for issue #11210
 }
